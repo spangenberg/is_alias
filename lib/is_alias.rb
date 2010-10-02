ddd180244017332b72f088559cc1f837ba701556
Daniel Spangenberg <daniel.spangenberg@gmail.com>
2010-10-02 19:16:57 +0200

    Modified the README, created gemspec, added application logic.

---
+++ lib/is_alias.rb
@@ -0,0 +1,20 @@
+module IsAlias
+  def self.included(base)
+    base.extend DanielSpangenberg::IsAlias::ClassMethods
+  end
+
+  module ClassMethods
+    def is_alias(methods)
+      methods.each do |name, method|
+        method = name if method.class == TrueClass
+        class_eval <<-METHODS, __FILE__, __LINE__ + 1
+          def is_#{name.to_s}?
+            self.#{method.to_s}
+          end
+        METHODS
+      end
+    end
+  end
+end
+
+ActiveRecord::Base.send(:include, IsAlias)
